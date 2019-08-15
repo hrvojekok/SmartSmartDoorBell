e6f2f2b693fff0a2d554b5dd0ae4461967f9e185
Hrvoje Kokosarevic <hrvoje.kok@gmail.com>
2019-08-15 22:36:53 +0200

    started with notifications

---
+++ app/src/main/java/com/example/smartdoorbell/FirebaseInstanceService.java
@@ -0,0 +1,25 @@
+package com.example.smartdoorbell;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.support.constraint.Constraints.TAG;
+
+public class FirebaseInstanceService extends FirebaseInstanceIdService {
+
+    private static final String REG_TOKEN = "REG_TOKEN";
+
+    @Override
+    public void onTokenRefresh() {
+
+        String recentToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("TOKEN", recentToken);
+        Toast.makeText(getApplicationContext(),recentToken, Toast.LENGTH_LONG);
+
+
+    }
+}
